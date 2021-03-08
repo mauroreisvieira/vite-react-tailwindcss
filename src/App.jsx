4ab3e4e43f4c00315ceb9299eeac97c07f235372
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-03-08 17:24:36 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './tailwind.css';
+
+function App() {
+  return (
+    <div>
+      <p className="text-red-500">Vite + React + TailwindCSS</p>
+    </div>
+  );
+}
+
+export default App;
